389b5f388cc75706af115f81ca5754141301061d
mengziya <115340408+mengziya@users.noreply.github.com>
2022-10-09 11:10:17 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -11,57 +11,33 @@ const USER_CONFIG = {
   APP_ID: 'wxbf445b54658b0dbc',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '7fe619dc6a8517c0bf9e55cdd238bd9a',
+  APP_SECRET: '544efd317a0526901ef88e50a5a7603a',
 
   PROVINCE: '江西',
-  CITY: '南昌',
+  CITY: '进贤',
 
   USERS: [
     {
       // 想要发送的人的名字
-      name: '梦梦',
+      name: '梦梦姐姐',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oLR826nbhHyKyemvScjN7Pq5a_dU',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '2hTSeOaBVmctH4uF0Zt34aMETdg29E0D_nSyOpor3bg',
+      useTemplateId: 'bymN5Vo-Ol1d4hEwYNK1hCFVhX7PLcOsOuVWnJ1F9t4',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '01-04',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '梦梦', year: '1998', date: '11-17',
+          type: '*生日', name: '梦梦的农历生日', year: '1998', date: '11-17,
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '梦梦', year: '1999', date: '01-04',
+          type: '生日', name: '梦梦的阳历生日', year: '1999', date: '01-04',
         },
-        {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
-        },
-      ],
-      // 我们在一起已经有xxxx天了的配置
-      customizedDateList: [
-        // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
-      ],
     },
-  ],
-
-
-  // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '2hTSeOaBVmctH4uF0Zt34aMETdg29E0D_nSyOpor3bg',
-
-  CALLBACK_USERS: [
-    {
-      name: '自己',
-      // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oLR826nbhHyKyemvScjN7Pq5a_dU',
-    }
   ],
 
 }
 
 module.exports = USER_CONFIG
-
